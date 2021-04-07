db51fffc34c2a4fd8716d5ba2b62bec3f86e4ba8
KeenWarrior <anujgargcse@gmail.com>
2021-04-07 17:59:36 +0530

    com

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "https://fierce-citadel-75064.herokuapp.com/";
 
 function App() {
+  const [response, setResponse] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const [username, setUsername] = useState("");
+  const [text, setText] = useState("");
+  const [socket, setSocket] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button
+        onClick={function () {
+          const socketVar = socketIOClient(ENDPOINT);
+          setSocket(socketVar);
+
+          socketVar.on("connect", (data) => {
+            setResponse("connected");
+          });
+
+          socketVar.on("message", (payload) => {
+            console.log(payload);
+          });
+
+        }}
+      >
+        {response ? response : "Click me"}
+      </button>
+
+      <input value={text} onChange={function(event){
+        setText(event.target.value);
+      }}></input>
+      <button onClick={function(){
+        socket.emit("message", {text});
+      }}>Send</button>
+    </React.Fragment>
   );
 }
 
